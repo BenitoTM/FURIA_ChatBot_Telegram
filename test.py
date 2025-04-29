1c4983e8882ff685d363710798b222dc498915cc
Benito Marino <betoniolomarino@gmail.com>
2025-04-29 16:57:54 -0300

    removendo_rasa_
    gradio apontando direto para o llm

--- test.py
+++ test.py
@@ -1,12 +1,6 @@
-import gradio as gr
 import requests
-import os
-
-
-RASA_API = os.getenv("RASA_API", "https://furia-chatbot-telegram-docker.onrender.com/webhooks/rest/webhook")
 SERVER_DADOS = "https://furiaservidordedados-production.up.railway.app/llm"
 
-# Função que envia mensagem para o Rasa
 def process_message(user_message, sender_id="test_user"):
     try:
         """response = requests.post(
@@ -16,9 +10,10 @@ def process_message(user_message, sender_id="test_user"):
         response = requests.post(
             SERVER_DADOS,
             json={
-                "question": f"{user_message.strip()} (sobre o time de CS:GO da FURIA)."
-            }
+                  "question": user_message
+                }
         )
+
 
         if response.ok:
             mensagens = response.json()
@@ -29,11 +24,4 @@ def process_message(user_message, sender_id="test_user"):
     except Exception as e:
         return f"Erro: {str(e)}"
 
-# Lançar interface Gradio
-def launch_interface():
-    gr.ChatInterface(
-        fn=process_message,
-        title="Chatbot FURIA CS2",
-        description="Pergunte qualquer coisa sobre o time da FURIA de CS.",
-        theme="soft"
-    ).launch(server_name="0.0.0.0", server_port=8000)
+print(process_message("Qual a lineup atual da FURIA de CS?"))
